21cb68afc9e6903787da6092560e7992e197eba8
luisn <lenegringarcia@gmail.com>
2021-01-14 10:11:39 -0500

    change docker-command

--- app/core/models.py
+++ app/core/models.py
@@ -2,12 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
-# Create your models here.
-
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        #Create and save a new user
+        '#Create and save a new user'
         if not email:
             raise ValueError('User must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
@@ -17,7 +15,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        #Create and save a new super user
+        '#Create and save a new super user'
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -26,7 +24,8 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
-    #Custom user model that suportrs using email instead of username
+
+    '#Custom user model that suportrs using email instead of username'
     email = models.EmailField(max_length=255, unique = True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default= True)
